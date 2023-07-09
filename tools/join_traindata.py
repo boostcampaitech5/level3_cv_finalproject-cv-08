47e25d396251805f86632306ba87b5e79a5caa65
lijm1358 <lijm1358@gmail.com>
2023-07-09 11:33:20 +0000

    feat: add train directory join tool

---
+++ tools/join_traindata.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+
+def main(args):
+    for path in args.data_paths:
+        for filename in os.listdir(path):
+            link_target = os.path.join(args.target_path, filename)
+            link_original = os.path.join(path, filename)
+            os.system(f'ln -s "{link_original}" "{link_target}"')
+        
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--data_paths', type=str, nargs='+')
+    parser.add_argument('--target_path', type=str)
+    
+    args = parser.parse_args()
+    
+    main(args)
