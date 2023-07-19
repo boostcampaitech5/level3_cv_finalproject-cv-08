7b5b81dbcb7856ca971cb0ce72b603ff2d088ed7
Kang Nahoon <ejrtks102020@gmail.com>
2023-07-19 03:01:35 +0000

    feat: add sheet generation

--- frontend.py
+++ frontend.py
@@ -18,7 +18,8 @@ from models.video_to_roll import resnet18
 
 import streamlit as st
 from streamlit import session_state as state
-
+from music21 import converter
+import glob
 
 @st.cache_resource
 def video_to_roll_load_model(device):
@@ -66,6 +67,10 @@ if __name__ == "__main__":
     if "input_video" not in state: state.input_video = None
     
     if "input_video" not in state: state.input_video = None
+    if "roll" not in state: state.roll = None
+    if "roll_wav" not in state: state.roll_wav = None
+    if "midi_wav" not in state: state.midi_wav = None
+    if "submit" not in state: state.submit = False
 
     st.header("Inference")    
     st.subheader("How to upload ?")
@@ -90,6 +95,7 @@ if __name__ == "__main__":
         
     with tab_url:
         # https://youtu.be/_3qnL9ddHuw
+        # https://www.youtube.com/watch?v=HB8-w5CvMls
         input_url = st.text_input(label="URL", placeholder="📂 Input youtube url here (ex. https://youtu.be/...)")
 
         if input_url:           
@@ -97,10 +103,12 @@ if __name__ == "__main__":
                 try:
                     if state.prev_url != input_url:
                         state.prev_url = input_url
+                        state.submit = False
                         with st.spinner("Url Analyzing ..."):
                             yt = YouTube(input_url)
                             yt.streams.filter(file_extension="mp4", res="720p").order_by("resolution").desc().first().download(output_path="./data/inference", filename="01.mp4")
-                except:
+                except Exception as e:
+                    print(e)
                     st.error("Please check Youtube url !")
                 else:
                     probe = ffmpeg.probe("./data/inference/01.mp4")
@@ -111,31 +119,72 @@ if __name__ == "__main__":
                     video_info['video_select_frame'] = video_info['video_select_range'][1] * video_info['video_fps'] - video_info['video_select_range'][0] * video_info['video_fps']
                     
                     url_submit = st.button(label="Submit", key="url_submit")
-                    if url_submit:
-                        s_t = time.time()
-                        with st.spinner("Data Preprocessing ..."):
-                            frames_with5 = preprocess(piano_detection_model, video_info, key='url')
-                        print(time.time()-s_t)
-                        preprocess_success_msg = st.success("Data Preprocessed Successfully!")
+                    if url_submit or state.submit:
+                        if url_submit:
+                            state.submit = True
+                            s_t = time.time()
+                            with st.spinner("Data Preprocessing ..."):
+                                frames_with5 = preprocess(piano_detection_model, video_info, key='url')
+                            print(time.time()-s_t)
+                            preprocess_success_msg = st.success("Data Preprocessed Successfully!")
+                            
+                            with st.spinner("Roll Data Inferencing ..."):
+                                roll, logit, roll_wav, pm_roll = video_to_roll_inference(video_to_roll_model, video_info, frames_with5)
+                            roll_inference_success_msg = st.success("Data Inferenced successfully!")
+                            
+                            with st.spinner("Midi Data Inferencing ..."):
+                                midi_wav, pm_midi = roll_to_midi_inference(roll_to_midi_model, logit)
+                            midi_inference_success_msg = st.success("Data Inferenced successfully!")
+                            
+                            state.roll = roll
+                            state.roll_wav = roll_wav
+                            state.midi_wav = midi_wav
+                            time.sleep(1)
+                            preprocess_success_msg.empty()
+                            time.sleep(0.5)
+                            roll_inference_success_msg.empty()
+                            time.sleep(0.5)
+                            midi_inference_success_msg.empty()
                         
-                        with st.spinner("Roll Data Inferencing ..."):
-                            roll, logit, roll_wav, _ = video_to_roll_inference(video_to_roll_model, video_info, frames_with5)
-                        roll_inference_success_msg = st.success("Data Inferenced successfully!")
-                        
-                        with st.spinner("Midi Data Inferencing ..."):
-                            midi_wav = roll_to_midi_inference(roll_to_midi_model, logit)
-                        midi_inference_success_msg = st.success("Data Inferenced successfully!")
-                        
-                        time.sleep(1)
-                        preprocess_success_msg.empty()
-                        time.sleep(0.5)
-                        roll_inference_success_msg.empty()
-                        time.sleep(0.5)
-                        midi_inference_success_msg.empty()
-                        
-                        st.image(np.rot90(roll, 1), width=700)
-                        st.audio(roll_wav, sample_rate=16000)
-                        st.audio(midi_wav, sample_rate=16000)
+                        st.image(np.rot90(state.roll, 1), width=700)
+                        st.audio(state.roll_wav, sample_rate=16000)
+                        st.audio(state.midi_wav, sample_rate=16000)
+                        if url_submit:
+                            with st.spinner("Generating Sheet ..."):
+                                output_dir = "./data/outputs"
+                                os.makedirs(output_dir, exist_ok=True)
+                                output_roll_midi_path = os.path.join(output_dir, "pm_roll.midi")
+                                output_midi_path = os.path.join(output_dir, "pm.midi")
+                                pm_roll.write(output_roll_midi_path)
+                                pm_midi.write(output_midi_path)
+
+                                roll_score = converter.parse(output_roll_midi_path)
+                                midi_score = converter.parse(output_midi_path)
+                                roll_pdf = os.path.join(output_dir, "roll_sheet")
+                                roll_png = os.path.join(output_dir, "roll_sheet")
+                                midi_pdf = os.path.join(output_dir, "midi_sheet")
+                                converter.subConverters.ConverterLilypond().write(roll_score, fmt='png', fp=roll_png, subformats='png')
+                                converter.subConverters.ConverterLilypond().write(roll_score, fmt='pdf', fp=roll_pdf, subformats='pdf')
+                                converter.subConverters.ConverterLilypond().write(midi_score, fmt='pdf', fp=midi_pdf, subformats='pdf')
+                                for file in glob.glob(output_dir + "/*[!pdf|!png]"):
+                                    os.remove(file)
+
+                        st.image("./data/outputs/roll_sheet.png")
+                        state.download_click = st.download_button(
+                            label="pdf download",
+                            data=open("./data/outputs/roll_sheet.pdf", 'rb').read(),
+                            file_name="piano_sheet.pdf",
+                            mime="application/pdf"
+                        )
+                        state.download_click = st.download_button(
+                            label="png download",
+                            data=open("./data/outputs/roll_sheet.png", 'rb').read(),
+                            file_name="piano_sheet.png",
+                            mime="image/png"
+                        )
+
+
+
                 
             else:
                 st.error("Please input url !")
@@ -161,11 +210,11 @@ if __name__ == "__main__":
                 preprocess_success_msg = st.success("Data Preprocessed Successfully!")
                     
                 with st.spinner("Roll Data Inferencing ..."):
-                    roll, logit, roll_wav, _ = video_to_roll_inference(video_to_roll_model, video_info, frames_with5)
+                    roll, logit, roll_wav, pm_roll = video_to_roll_inference(video_to_roll_model, video_info, frames_with5)
                 roll_inference_success_msg = st.success("Data Inferenced successfully!")
                 
                 with st.spinner("Midi Data Inferencing ..."):
-                    midi_wav = roll_to_midi_inference(roll_to_midi_model, video_info, logit)
+                    midi_wav, pm_midi = roll_to_midi_inference(roll_to_midi_model, logit)
                 midi_inference_success_msg = st.success("Data Inferenced successfully!")
                 
                 time.sleep(1)
